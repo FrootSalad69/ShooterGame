55e225c7804b100eb5d644797b96aaa3e3ad6a94
bluephaser <liam.binford@digipen.edu>
2020-08-15 19:23:04 -0700

    thdtghdrtggdrgdrgdargartgaergse

---
+++ First-Person Game/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,73 @@
+﻿/*
+ CameraMove.cs
+ By: Liam Binford
+ Date: 7/28/20
+ Description: Allow the player to control the rotation of the camera by moving the mouse
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    //declare variables
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    private void LockCursor()
+    {
+        //make sure that the cursor does not move when the camera is being moved
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        //create values based on the movement of the mouse multiplied by the sensitivity
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        //have the x axis clamp monitor the current position of hte camera
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        //rotate the camera based on the up/down movements of the mouse
+        transform.Rotate(Vector3.left * mouseY);
+        //rotate the player and camera based on the left/right movements of the mouse
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ First-Person Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,74 @@
+﻿/*
+ PlayerMove.cs
+ By: Liam Binford
+ Date: 7/28/20
+ Description: Allows the player to move the cahracter using WASD or Arrow Keys
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float speed;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float vertInput = Input.GetAxis(verticalInputName) * speed;
+        float horizInput = Input.GetAxis(horizontalInputName) * speed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput();
+    }
+
+    private void JumpInput()
+    {
+        if(Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float airTime = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(airTime);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            airTime += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
